b7d250456aec5cca88ea6467ebfb98b3753f5e34
MichPlewa <michalplewa02@gmail.com>
2023-08-01 13:45:16 +0200

    add jump

--- dasher.cpp
+++ dasher.cpp
@@ -4,7 +4,17 @@ int main(){
     const int windowWidth = 512;
     const int windowHeight = 380;
 
+
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    const int gravity = 1;
+    const int jumpVal = -22;
+
+    const int width = 50;
+    const int height = 80;
+
+    int posY = windowHeight - height;
+    int velosity = 0;
 
     SetTargetFPS(60);
 
@@ -13,6 +23,18 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
+        if(posY >= windowHeight - height){
+            velosity = 0;
+            if(IsKeyPressed(KEY_SPACE)){
+                velosity += jumpVal;
+            }
+        }else {
+            velosity += gravity;
+        }
+
+        posY += velosity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
 
         EndDrawing();
     }
